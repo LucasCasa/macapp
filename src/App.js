f9e5d33d7dccb7609382b2a4752b6e678838cf18
lucascasa <pichoto1>
2022-12-19 08:17:01 -0300

    icon

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <section className="header">
+      <div className="title-wrapper">
+        <h1 className="sweet-title waviy">
+          <span styles="--i:2" data-text="Felicitaciones">Felicitaciones</span>
+          <span styles="--i:3" data-text="Licenciada">Licenciada</span>
+        </h1>
+        <span className="top-title"></span>
+        <span className="bottom-title"></span>
+      </div>
+    </section>
+  );
+}
+
+export default App;
